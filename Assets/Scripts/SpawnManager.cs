b91fe3260b1d6fdb3d13a7a3c501cc5186a1ec89
khaled-74 <princek_59@yahoo.com>
2022-12-13 03:33:40 +0200

    spawn cubes with random angel and more polishing

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -10,7 +10,7 @@ public class SpawnManager : MonoBehaviour
     public float startDelay = 2;
     public float spawnInterval = 3f;
 
-    public GameObject hand;
+
     private void Start()
     {
         InvokeRepeating("SpawnRandomCube", startDelay, spawnInterval);
@@ -23,7 +23,44 @@ public class SpawnManager : MonoBehaviour
     {
         int cubeIndex = Random.Range(0, cubeType.Length);
         int posIndex = Random.Range(0, spawnPos.Length);
-        Instantiate(cubeType[cubeIndex], spawnPos[posIndex].position, cubeType[cubeIndex].transform.rotation);
+        Quaternion cubeRotation = Quaternion.Euler(0, 0, 90 * Random.Range(0,4));
+
+        Instantiate(cubeType[cubeIndex], spawnPos[posIndex].position, cubeRotation);
     }
 
+}
+
+// Rotate a GameObject using a Quaternion.
+// Tilt the cube using the arrow keys. When the arrow keys are released
+// the cube will be rotated back to the center using Slerp.
+
+public class ExampleScript : MonoBehaviour
+{
+    float smooth = 5.0f;
+    float tiltAngle = 60.0f;
+
+    void Update()
+    {
+        // Smoothly tilts a transform towards a target rotation.
+        float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+        float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
+
+        // Rotate the cube by converting the angles into a quaternion.
+        Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+
+        // Dampen towards the target rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+    }
+
+    public class Example2 : MonoBehaviour
+    {
+        float rotateSpeed = 90;
+
+        // Applies a rotation of 90 degrees per second around the Y axis
+        void Update()
+        {
+            float angle = rotateSpeed * Time.deltaTime;
+            transform.rotation *= Quaternion.AngleAxis(angle, Vector3.up);
+        }
+    }
 }
